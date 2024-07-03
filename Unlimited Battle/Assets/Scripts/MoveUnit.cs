13628188369ebc7858be19d52545be53cc651798
Qutigara <maks1234567890123456789012@mail.ru>
2024-07-03 20:29:01 +0300

    V 1.1.4a
    1. Реализовали систему Тумана войны
    2. Заменили модель юнита со скелета на рыцаря
    3. Реализовали проигрывание анимации Walk при передвижении юнита (пока багает)

--- Unlimited Battle/Assets/Scripts/MoveUnit.cs
+++ Unlimited Battle/Assets/Scripts/MoveUnit.cs
@@ -7,22 +7,54 @@ public class MoveUnit : MonoBehaviour
 {
     private Camera mainCamera;
     private NavMeshAgent agent;
+    private Animator anim;
+    private bool isAtDestination;
+    private bool isAtDestination2;
 
 
     private void Start()
     {
+        
         mainCamera = Camera.main;
         agent = GetComponent<NavMeshAgent>();
+        anim = GetComponentInChildren<Animator>();
+        
     }
     void Update()
     {
-        if (Input.GetMouseButtonDown(1)) 
+        if (Input.GetMouseButtonDown(1))
         {
+            isAtDestination2 = true;
+            anim.SetFloat("Speed", 1);
             RaycastHit hit;
-            if (Physics.Raycast(mainCamera.ScreenPointToRay(Input.mousePosition), out hit) )
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit))
             {
+                anim.SetFloat("Speed", 1);
                 agent.SetDestination(hit.point);
+                isAtDestination = false;
+                Debug.Log("Проиграна анимация walk");
+            }
+        }
+
+        if (isAtDestination2)
+        {
+            // Проверка, достигла ли цель назначения
+            if (agent.remainingDistance <= agent.stoppingDistance)
+            {
+
+                if (!isAtDestination)
+                {
+                    isAtDestination2 = false;
+                    Debug.Log("Проиграна анимация stand");
+                    // Вызов анимации после достижения цели
+                    anim.SetFloat("Speed", 0);
+                    isAtDestination = true;
+                }
             }
         }
     }
+
+
+
+
 }
