195af8fc755a76d70695e2211a619cced0dc102f
DenosScarlet <dienis.davydov.2003@mail.ru>
2024-07-03 16:19:04 +0300

    V 1.1.3 a
    Переделал NavSurface
    
    Отметил все деревья как NavModifiedЧётоТам
    
    Сделал обход объектов игроком

--- Unlimited Battle/Assets/Scripts/MoveUnit.cs
+++ Unlimited Battle/Assets/Scripts/MoveUnit.cs
@@ -5,48 +5,23 @@ using UnityEngine.AI;
 
 public class MoveUnit : MonoBehaviour
 {
-    // Скорость передвижения юнита
-    public float moveSpeed = 5f;
+    private Camera mainCamera;
+    private NavMeshAgent agent;
 
-    // Целевая точка для передвижения
-    private Vector3 targetPosition;
 
-    // Флаг, указывающий, находится ли юнит в движении
-    private bool isMoving = false;
-
+    private void Start()
+    {
+        mainCamera = Camera.main;
+        agent = GetComponent<NavMeshAgent>();
+    }
     void Update()
     {
-        // Проверка нажатия правой кнопки мыши
-        if (Input.GetMouseButtonDown(1))
+        if (Input.GetMouseButtonDown(1)) 
         {
-            // Получение позиции курсора мыши в мировых координатах
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
-
-            // Проверка, попал ли луч по коллайдеру
-            if (Physics.Raycast(ray, out hit))
+            if (Physics.Raycast(mainCamera.ScreenPointToRay(Input.mousePosition), out hit) )
             {
-                targetPosition = hit.point;
-
-                // Установка флага, что юнит находится в движении
-                isMoving = true;
-            }
-        }
-
-        // Перемещение юнита к цели, если он в движении
-        if (isMoving)
-        {
-            // Движение юнита к цели
-            transform.position = Vector3.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
-
-            Vector3 direction = targetPosition - transform.position;
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), 5f * Time.deltaTime);
-
-            // Проверка достижения цели
-            if (Vector3.Distance(transform.position, targetPosition) < 0.1f)
-            {
-                // Установка флага, что юнит не в движении
-                isMoving = false;
+                agent.SetDestination(hit.point);
             }
         }
     }
